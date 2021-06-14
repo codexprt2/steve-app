49ff56c96094691ad9de57cc98fb72bceac807ae
Anjaliba Gohil <codexprt2@gmail.com>
2021-06-14 18:36:54 +0530

    structure done

--- components/CardTwo.js
+++ components/CardTwo.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "../styles/CardTwo.module.css";
 
 const CardTwo = () => {
-	return <div>Card Two</div>;
+	return (
+		<div className={styles.cardContainer}>
+			<div>Image Div</div>
+			<div>
+				<h1>Carlos Smith</h1>
+				<h3>Full Stack Developer</h3>
+			</div>
+			<div>
+				<div>Home</div>
+				<div>About</div>
+				<div>PortFolio</div>
+				<div>Blog</div>
+				<div>Contact</div>
+			</div>
+		</div>
+	);
 };
 
 export default CardTwo;
