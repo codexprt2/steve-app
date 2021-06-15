816d18c8fbe7205a6dd55f0bf207637a6ca6df6b
Anjaliba Gohil <codexprt2@gmail.com>
2021-06-15 13:01:27 +0530

    btn icon added

--- components/CardOne.js
+++ components/CardOne.js
@@ -1,5 +1,17 @@
 import React from "react";
 import styles from "../styles/CardOne.module.css";
+import { CgFacebook, CgLoadbarAlt } from "react-icons/cg";
+import { IconContext } from "react-icons";
+import { FaVimeoSquare } from "react-icons/fa";
+import { IoIosArrowDropdown } from "react-icons/io";
+import {
+	AiOutlineGoogle,
+	AiOutlineTwitter,
+	AiOutlineInstagram,
+	AiFillLinkedin,
+	AiFillGithub,
+	AiFillYoutube,
+} from "react-icons/ai";
 
 const CardOne = () => {
 	return (
@@ -7,20 +19,57 @@ const CardOne = () => {
 			<div className={styles.heading}>
 				<div className={styles.content}>
 					<h1>Carlos Smith</h1>
-					<h3>Description</h3>
+					<p>Description</p>
 				</div>
 				<div className={styles.btnContainer}>
-					<div>F</div>
-					<div>G</div>
-					<div>T</div>
-					<div>I</div>
-					<div>L</div>
-					<div>Git</div>
-					<div>V</div>
-					<div>Y</div>
+					<div className={styles.btnContent}>
+						<CgFacebook />
+					</div>
+					<div className={styles.btnContent}>
+						<AiOutlineGoogle />
+					</div>
+					<div className={styles.btnContent}>
+						<AiOutlineTwitter />
+					</div>
+					<div className={styles.btnContent}>
+						<AiOutlineInstagram />
+					</div>
+					<div className={styles.btnContent}>
+						<AiFillLinkedin />
+					</div>
+					<div className={styles.btnContent}>
+						<AiFillGithub />
+					</div>
+					<div className={styles.btnContent}>
+						<FaVimeoSquare />
+					</div>
+					<div className={styles.btnContent}>
+						<AiFillYoutube />
+					</div>
+				</div>
+
+				<IconContext.Provider
+					value={{
+						size: "4rem",
+					}}>
+					<div className={styles.lineContainer}>
+						<CgLoadbarAlt />
+					</div>
+				</IconContext.Provider>
+			</div>
+
+			<div className={styles.downloadContent}>
+				<div className={styles.cvContainer}>
+					<div className={styles.btnContent}>
+						<IoIosArrowDropdown />
+					</div>
+					<div>Download CV</div>
+				</div>
+				<div className={styles.printContainer}>
+					<div>Button</div>
+					<div>Download CV</div>
 				</div>
 			</div>
-			<div></div>
 		</div>
 	);
 };
