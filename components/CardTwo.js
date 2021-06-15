111b74442979f897096a0c3c9c80c054527b1d97
Anjaliba Gohil <codexprt2@gmail.com>
2021-06-15 17:42:00 +0530

    card2 styling done

--- components/CardTwo.js
+++ components/CardTwo.js
@@ -1,20 +1,89 @@
 import React from "react";
 import styles from "../styles/CardTwo.module.css";
+import { HiHome } from "react-icons/hi";
+import { ImUser } from "react-icons/im";
+import { GrCamera } from "react-icons/gr";
+import { TiMessages } from "react-icons/ti";
+import { CgMail } from "react-icons/cg";
+
+import Image from "next/image";
+import { style } from "@material-ui/system";
 
 const CardTwo = () => {
 	return (
 		<div className={styles.cardContainer}>
-			<div>Image Div</div>
-			<div>
-				<h1>Carlos Smith</h1>
-				<h3>Full Stack Developer</h3>
+			<div className={styles.imgDiv}>
+				<div className={styles.profileImage}>
+					<div className={styles.profileImageBorder}>
+						<Image
+							class={styles.roundedCircle}
+							src='/assets/img/profile.jpeg'
+							width={500}
+							height={500}
+						/>
+					</div>
+				</div>
 			</div>
-			<div>
-				<div>Home</div>
-				<div>About</div>
-				<div>PortFolio</div>
-				<div>Blog</div>
-				<div>Contact</div>
+			<div className={styles.titleContainer}>
+				<h2>Carlos Smith</h2>
+				<p>Full Stack Developer</p>
+			</div>
+			<div className={styles.pageContainer}>
+				<a href='/home'>
+					<div className={styles.homeContainer}>
+						<div style={{ alignItems: "center" }}>
+							<span className={styles.element}>Home</span>
+						</div>
+
+						<div>
+							<HiHome size={20} />
+						</div>
+					</div>
+				</a>
+				<a href='/about'>
+					<div className={styles.homeContainer}>
+						<div style={{ alignItems: "center" }}>
+							<span className={styles.element}>About</span>
+						</div>
+
+						<div>
+							<ImUser size={20} />
+						</div>
+					</div>
+				</a>
+				<a href='/portfolio'>
+					<div className={styles.homeContainer}>
+						<div style={{ alignItems: "center" }}>
+							<span className={styles.element}>PortFolio</span>
+						</div>
+
+						<div>
+							<GrCamera size={20} />
+						</div>
+					</div>
+				</a>
+				<a href='/blog'>
+					<div className={styles.homeContainer}>
+						<div style={{ alignItems: "center" }}>
+							<span className={styles.element}>Blog</span>
+						</div>
+
+						<div>
+							<TiMessages size={20} />
+						</div>
+					</div>
+				</a>
+				<a href='/contact'>
+					<div className={styles.homeContainer}>
+						<div style={{ alignItems: "center" }}>
+							<span className={styles.element}>Contact</span>
+						</div>
+
+						<div>
+							<CgMail size={20} />
+						</div>
+					</div>
+				</a>
 			</div>
 		</div>
 	);
