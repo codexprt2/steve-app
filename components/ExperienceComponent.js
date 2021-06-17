06776dc252c586e196ea926ee2aec8bd1abcf3b7
Anjaliba Gohil <codexprt2@gmail.com>
2021-06-17 13:58:02 +0530

    Service component start

--- components/ExperienceComponent.js
+++ components/ExperienceComponent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./EXPERIENCE.module.css";
 import { RiBriefcase4Line } from "react-icons/ri";
-import ProgressBar from "./ProgressBar";
 
 const ExperienceComponent = () => {
 	return (
--- components/ServiceContent.js
+++ components/ServiceContent.js
@@ -1,12 +1,55 @@
 import React from "react";
 import styles from "./SERVICE.module.css";
+import { RiBriefcase4Line } from "react-icons/ri";
 
 const ServiceContent = () => {
 	return (
 		<div className={styles.mainContainer}>
-			{/* <div>Hello</div>
-			<p>hi hi hi</p>
-			<div>Hiii</div> */}
+			<div className={styles.mainContent}>
+				<div className={styles.contentDiv}>
+					<div className={styles.iconContainer}>
+						<div className={styles.iconOutter}>
+							<div className={styles.iconInner}>
+								<RiBriefcase4Line size={25} />
+							</div>
+						</div>
+					</div>
+					<div className={styles.title}>Great Support</div>
+					<div>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium.
+					</div>
+				</div>
+				<div className={styles.contentDiv}>
+					<div className={styles.iconContainer}>
+						<div className={styles.iconOutter}>
+							<div className={styles.iconInner}>
+								<RiBriefcase4Line size={25} />
+							</div>
+						</div>
+					</div>
+					<div className={styles.title}>Great Support</div>
+					<div>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium.
+					</div>
+				</div>
+				<div className={styles.contentDiv}>
+					<div className={styles.iconContainer}>
+						<div className={styles.iconOutter}>
+							<div className={styles.iconInner}>
+								<RiBriefcase4Line size={25} />
+							</div>
+						</div>
+					</div>
+
+					<div className={styles.title}>Great Support</div>
+					<div>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium.
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
